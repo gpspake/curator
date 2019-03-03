7e55980b40d64e0fee7af65b92046cc53325cc5e
George Spake <gpspake@gmail.com>
2019-03-03 10:40:55 -0600

    devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+
   post "/graphql", to: "graphql#execute"
+
   resources :articles do
     resources :comments
   end
 
   resources :posters
+
+  root to: "home#index"
 end
